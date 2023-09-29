aa37d1a950726acd344edfbc5176c8523381cace
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-09-29 14:48:43 +0800

    7-create-simplecard-component (#21)

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,22 +1,51 @@
 <script lang="ts">
 	import SimpleButton from '$lib/components/primitive/SimpleButton.svelte';
+	import SimpleCard from '$lib/components/primitive/SimpleCard.svelte';
+	import Modularized from '$lib/assets/misc/cards/modularized.svg';
+	import ProjectBased from '$lib/assets/misc/cards/project-based.svg';
+	import IndustryRelevant from '$lib/assets/misc/cards/industry-relevant.svg';
 	function handleButtonClick() {
 		console.log('yep');
 	}
 </script>
 
-<div>
+<h1>Test Buttons</h1>
+<section>
 	<SimpleButton onclick={handleButtonClick} text="Primary" />
 	<SimpleButton onclick={handleButtonClick} elementType="secondary" text="Secondary" />
 	<SimpleButton onclick={handleButtonClick} elementType="secondary" text="Secondary" mode="dark" />
-</div>
+</section>
+<h1>Test Simple Cards</h1>
+<section>
+	<ul>
+		<li>
+			<SimpleCard src={Modularized} alt="Modularized" title="Modularized" />
+		</li>
+		<li>
+			<SimpleCard src={ProjectBased} alt="Project Based" title="Project Based" />
+		</li>
+		<li>
+			<SimpleCard src={IndustryRelevant} alt="Industry Relevant" title="Industry Relevant" />
+		</li>
+	</ul>
+</section>
 
 <style>
-	div {
+	h1 {
+		text-align: center;
+	}
+	section {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		height: 100vh;
+		margin: 2rem;
+	}
+	ul {
+		display: flex;
+		gap: 32px;
+	}
+	li {
+		list-style: none;
 	}
 </style>
